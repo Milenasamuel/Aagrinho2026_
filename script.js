5278372373366b7bb03f332673a3ad6e9ce40edc
milena samuel <milena.samuel.goncalves@escola.pr.gov.br>
2026-06-18 19:02:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+// Controle do Tamanho da Fonte
+let currentFontSize = 16;
+function alterarFonte(action) {
+    currentFontSize += action;
+    if(currentFontSize < 12) currentFontSize = 12;
+    if(currentFontSize > 26) currentFontSize = 26;
+    document.documentElement.style.setProperty('--font-size-base', currentFontSize + 'px');
+}
 
+// Controle de Brilho
+let currentBrightness = 100;
+function alterarBrilho(action) {
+    currentBrightness += action;
+    if(currentBrightness < 40) currentBrightness = 40;
+    if(currentBrightness > 160) currentBrightness = 160;
+    document.documentElement.style.setProperty('--brightness-level', currentBrightness + '%');
+}
+
+// Alternar entre Modo Claro / Escuro
+function toggleConteudo() {
+    document.body.classList.toggle('dark-mode');
+}
+
+// Sistema de Filtros de Daltonismo
+function aplicarDaltonismo(tipo) {
+    // Limpa filtros anteriores
+    document.body.classList.remove('deuteranopia', 'protanopia', 'tritanopia', 'acromatopsia');
+    
+    // Insere o novo filtro ativo
+    if (tipo !== 'normal') {
+        document.body.classList.add(tipo);
+    }
+}
